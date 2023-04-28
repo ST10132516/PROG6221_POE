98e946d1b046628ac8161ea7d107292fc620f3f6
ST10132516 <ST10132516@vcconnect.edu.za>
2023-04-28 14:37:16 +0200

    Second commit, created classes for Recipe and Ingredient

---
+++ SaneleApplication/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaneleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class Ingredient
+    {
+        public string name;
+        public double quantity;
+        public string unit;
+    }
+
+    class Recipe
+    {
+        private Ingredient[] ingredientsArr;
+        private string[] stepsArr;
+        private int numIngredients;
+        private int numSteps;
+        public Recipe()
+        {
+            ingredientsArr = new Ingredient[10]; // start with capacity for 10 ingredients
+            stepsArr = new string[10]; // start with capacity for 10 steps
+            numIngredients = 0;
+            numSteps = 0;
+        }
+
+
+
+    }
+}
